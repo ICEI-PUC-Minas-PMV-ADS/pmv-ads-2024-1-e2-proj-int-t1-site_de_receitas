d02d1434f1d4307e859c02f56d5801eb6df8b55b
Isabela Castro <91014344+isgcas@users.noreply.github.com>
2024-04-20 15:20:49 -0300

    razorpages

--- src/SiteReceitas/Program.cs
+++ src/SiteReceitas/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
