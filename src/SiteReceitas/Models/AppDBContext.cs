aba1e27b79097e94b8d0ef78a05b30ec94be3346
Lorrayne Caliari <lorraynevictoria272@gmail.com>
2024-04-23 19:50:40 -0300

    debugando

--- src/SiteReceitas/Models/AppDBContext.cs
+++ src/SiteReceitas/Models/AppDBContext.cs
@@ -5,12 +5,6 @@ namespace SiteReceitas.Models
     public class AppDBContext:DbContext
     {
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
-
-        public DbSet<Ingrediente> Ingredientes { get; set; }
-
-        public DbSet<Usuario> Usuarios { get; set; }
-
-        public DbSet<Receita> Receitas { get; set; }
-        public DbSet<Ingrediente_Receita> IngredientesReceita { get; set; }
+ 
     }
 }
