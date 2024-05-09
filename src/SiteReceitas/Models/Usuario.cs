64f0c3e6c59bcd1b4016459b2e0b6a7eeadc631a
Isabela Castro <91014344+isgcas@users.noreply.github.com>
2024-05-09 20:46:38 -0300

    Create Usuario.cs

---
+++ src/SiteReceitas/Models/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SiteReceitas.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+
+        [Key]
+        public int Id { set; get; }
+
+        [Required(ErrorMessage = "É necessário informar o nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar o sobrenome")]
+        [Display(Name = "Sobrenome")]
+        public string SobreNome { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar o E-mail")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail inválido")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Senha { get; set; }
+    }
+
+    public enum Perfil{
+        Admin,
+        User
+    }
+}
