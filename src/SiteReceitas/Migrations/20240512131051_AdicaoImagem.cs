3c6f00c8b2a5f0828e802dc365d859cb373ab68d
Alexandre Figueiredo <alexandrefigueiredocamargo3@gmail.com>
2024-05-12 10:33:46 -0300

    adciona atributo de imagem a receita

---
+++ src/SiteReceitas/Migrations/20240512131051_AdicaoImagem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SiteReceitas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicaoImagem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Imagem",
+                table: "Receitas",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Imagem",
+                table: "Receitas");
+        }
+    }
+}
