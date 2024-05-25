0644c2e5d9e8485944c11e2049f8dbe97b99c473
Matheus <matheus_castelliano@outlook.com>
2024-05-25 20:50:00 -0300

    Acertei alguns detalhes. Tentei resolver o create da receita, mas sem sucesso

--- src/SiteReceitas/Migrations/20240525234426_M05-UpdateTableReceitas.Designer.cs
+++ src/SiteReceitas/Migrations/20240525234426_M05-UpdateTableReceitas.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SiteReceitas.Models;
 
@@ -10,9 +11,11 @@ using SiteReceitas.Models;
 namespace SiteReceitas.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240525234426_M05-UpdateTableReceitas")]
+    partial class M05UpdateTableReceitas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
